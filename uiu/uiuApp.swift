356ca734f274bbec41062bab2edb31e3e2e5e727
raneem <ixranem7@gmail.com>
2024-09-26 01:51:04 -0700

    Initial Commit

---
+++ uiu/uiuApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiuApp.swift
+//  uiu
+//
+//  Created by Raneem on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct uiuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
